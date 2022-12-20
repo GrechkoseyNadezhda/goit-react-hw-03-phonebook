6427d5fb5a6d58ef68f6c8e934dc06293beae991
GrechkoseyNadezhda <Grechkosey.n@gmail.com>
2022-12-20 09:05:38 +0100

    homework-03-phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,85 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+import { Wrapper, Message } from './App.styled';
+
+import { ContactForm } from './ContactForm/ContactForm';
+import { Filter } from './Filter/Filter';
+import { ContactList } from './ContactList/ContactList';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  onFormSubmit = info => {
+    const isContactRepeat = this.state.contacts.find(
+      el => el.name === info.name
+    );
+    if (isContactRepeat) {
+      alert('Already in Contacts');
+      return;
+    }
+    const contact = {
+      ...info,
+      id: nanoid(),
+    };
+    this.setState(prevState => ({
+      contacts: [...prevState.contacts, contact],
+    }));
+  };
+
+  onInputChange = filter => {
+    this.setState({
+      filter,
+    });
+  };
+
+  filteredContacts = () => {
+    const { contacts, filter } = this.state;
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(filter.toLowerCase())
+    );
+  };
+
+  deleteToDo = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+
+  render() {
+    const filteredContacts = this.filteredContacts();
+    return (
+      <Wrapper>
+        <h1>Phonebook</h1>
+        <ContactForm onFormSubmit={this.onFormSubmit} />
+        {this.state.contacts.length > 0 ? (
+          <>
+            <h2>Contacts</h2>
+            <Filter onInputChange={this.onInputChange} />
+            <ContactList list={filteredContacts} deleteToDo={this.deleteToDo} />
+          </>
+        ) : (
+          <Message>Contacts list is empty yet</Message>
+        )}
+      </Wrapper>
+    );
+  }
+}
---
+++ src/components/ContactList/ContactList.jsx
@@ -0,0 +1,35 @@
+import PropTypes from 'prop-types';
+import { List, Btn, Item } from './ContactList.styled';
+
+export const ContactList = props => {
+  const { list, deleteToDo } = props;
+  return (
+    <List>
+      {list.map(({ name, number, id }) => (
+        <Item key={id}>
+          <div>
+            {name}:{number}
+          </div>
+          <Btn type="button" onClick={() => deleteToDo(id)}>
+            delete
+          </Btn>
+        </Item>
+      ))}
+    </List>
+  );
+};
+
+ContactList.propTypes = {
+  list: PropTypes.arrayOf(PropTypes.number.isRequired).isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
+ContactList.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      number: PropTypes.string.isRequired,
+      id: PropTypes.string.isRequired,
+    }).isRequired
+  ).isRequired,
+  deleteToDo: PropTypes.func.isRequired,
+};
